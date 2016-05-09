fd952eeabe60cfa314c27f8554d031cd5b0c0623
Lukas Strassel <lukasstrassel@googlemail.com>
2016-05-10 00:25:39 +0200

    fix url

--- js/init.js
+++ js/init.js
@@ -1,9 +1,50 @@
 (function($){
   $(function(){
-
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+      closeOnClick: true
+    });
     $('.parallax').parallax();
-    $("#contactform").validate();
-
+    $("#contactform").validate({
+      lang: 'de'
+    });
+    $('a[href^="#"]').click(
+      function(e) {
+        e.preventDefault(); var dest = $(this).attr('href');
+        console.log(dest);
+        $('html,body').animate({ scrollTop: $(dest).offset().top }, 'slow');
+      }
+    );
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+/*jQuery(document).ready(function($) {
+  var siteUrl = 'http://'+(document.location.hostname||document.location.host);
+
+  // Make sure that all clicked links that link to your internal website
+  // don't just reload the page but execute a History.pushState call
+  $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
+    e.preventDefault();
+    History.pushState({}, "", this.pathname);
+  });
+
+  // Catch all History stateChange events
+  History.Adapter.bind(window, 'statechange', function(){
+    var State = History.getState();
+    // Load the new state's URL via an Ajax Call
+    $.get(State.url, function(data){
+      // Replace the "<title>" tag's content
+      var title = $(data).filter("title").text();
+      document.title = title;
+
+      // Replace the content of the main container (.content)
+      // If you're using another div, you should change the selector
+      var content = $(data).filter('body');
+      $('body').html(content);
+      $('.parallax').parallax();
+      $("#contactform").validate();
+      // If you're using Google analytics, make sure the pageview is registered!
+      //_gaq.push(['_trackPageview', State.url]);
+    });
+  });
+});
+  */
